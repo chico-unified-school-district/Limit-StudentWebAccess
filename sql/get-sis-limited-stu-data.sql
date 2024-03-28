3de135731f1de296ba4344fbcf7a3583fdfe45ba
Justin Cooper <jcooper@chicousd.org>
2024-03-28 10:58:18 -0700

    PBN days need to be fixed

--- sql/get-sis-limited-stu-data.sql
+++ sql/get-sis-limited-stu-data.sql
@@ -1,16 +1,25 @@
 SELECT DISTINCT
- STU.ID AS permId
-,STU.FN AS firstName
-,STU.LN AS lastName
-,STU.GR AS grade
-,DSP.DY AS dayCount
-,DSP.DD AS startDate
-,DSP.ED AS endDate
-,DSP.IUN AS staffUsr
-from DSP LEFT JOIN STU ON stu.id = dsp.pid
-where pid IS NOT NULL
-AND DSP.DS = 'PB'
-AND DY > 0
-AND (DSP.DEL = 0 OR DSP.DEL IS NULL)
-AND DSP.ED > getdate()
-AND dsp.dts > DATEADD(year, -1, getdate());
+  STU.ID AS permId
+ ,STU.FN AS firstName
+ ,STU.LN AS lastName
+ ,STU.GR AS grade
+ ,DSD.DY AS dayCount
+ ,DSD.DS AS pbType
+ ,LOG.USR AS staffUsr
+ ,DSD.DD AS startDate
+ ,NULL AS endDate
+FROM
+ DSD
+ LEFT JOIN STU ON STU.ID = DSD.ID
+ LEFT JOIN (
+ SELECT DISTINCT ID,USR FROM LOG
+ WHERE CD = 'DSDchg'
+ AND CO = 'District Supplemental - Submission'
+ ) AS LOG ON STU.ID = LOG.ID
+WHERE
+STU.TG = ' ' -- Active Students only!
+AND ( DSD.DS = 'PB' OR DSD.DS = 'PBN') -- PBN is Penalty Box No End Date
+AND (DSD.DEL IS NOT NULL OR DSD.DEL = 0)
+-- endDate greater than 1 days ago
+AND ( DSD.DD IS NULL OR DATEADD(day,DY,DSD.DD) > DATEADD(day,-1,GETDATE()) )
+;
